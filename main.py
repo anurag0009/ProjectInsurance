f44df272f975fd1ac021f1b6909723962bc8089a
anurag0009 <anurag.anurag.singh864@gmail.com>
2023-05-16 16:45:25 +0530

    exception and logger file added

---
+++ main.py
@@ -0,0 +1,26 @@
+from insurance.logger import logging
+from insurance.exception import InsuranceException
+import os
+import sys
+
+def test_logger_and_expection():
+        try:
+            logging.info("Starting the test_logger_and_exception")
+            result = 3/0
+            print(result)
+            logging.info("Stoping the test_logger_and_exception")
+        except Exception as e:
+            logging.debug(str(e))
+            raise InsuranceException(e,sys)
+        
+
+
+
+
+
+
+if __name__ =="__main__":
+     try:
+          test_logger_and_expection()
+     except Exception as e:
+          print(e)
